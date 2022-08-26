230431388d8899d044f5dce9b9ddd5c5f400e0fc
TexHaPb <texhapb@bk.ru>
2022-08-26 13:33:23 +0300

    add base template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,82 @@
 import './App.css';
+import Icons from './Icons';
 
 function App() {
-  return (
-    <div></div>
-  );
+	return (
+		<div className='app'>
+			<div className='container'>
+				<h1>To-Do List</h1>
+
+				<div className='add-form-block'>
+					<form className='add-form'>
+						<input type='text' name='new-task-name' className='add-form__input' placeholder="Add new task" />
+						<button type='submit' className='add-form__submit'>Add</button>
+					</form>
+				</div>
+
+				<div className='todo'>
+					<ul className='todo__list'>
+						<li className='todo__element'>
+							<div className='todo__content'>
+								<input type='checkbox' className='todo__checkbox' />
+								<span className='todo__title'>Buy smth</span>
+							</div>
+
+							<button className='todo__remove'>
+								<Icons name='bin' className='todo__remove-icon' />
+							</button>
+						</li>
+
+						<li className='todo__element done'>
+							<div className='todo__content'>
+								<input type='checkbox' checked className='todo__checkbox' />
+								<span className='todo__title'>Buy smth</span>
+							</div>
+
+							<button className='todo__remove'>
+								<Icons name='bin' className='todo__remove-icon' />
+							</button>
+						</li>
+
+						<li className='todo__element'>
+							<div className='todo__content'>
+								<input type='checkbox' className='todo__checkbox' />
+								<span className='todo__title'>Buy smth</span>
+							</div>
+
+							<button className='todo__remove'>
+								<Icons name='bin' className='todo__remove-icon' />
+							</button>
+						</li>
+
+						<li className='todo__element'>
+							<div className='todo__content'>
+								<input type='checkbox' className='todo__checkbox' />
+								<span className='todo__title'>Buy smth</span>
+							</div>
+
+							<button className='todo__remove'>
+								<Icons name='bin' className='todo__remove-icon' />
+							</button>
+						</li>
+
+						<li className='todo__element'>
+							<div className='todo__content'>
+								<input type='checkbox' className='todo__checkbox' />
+								<span className='todo__title'>Buy smth</span>
+							</div>
+
+							<button className='todo__remove'>
+								<Icons name='bin' className='todo__remove-icon' />
+							</button>
+						</li>
+					</ul>
+				</div>
+
+
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Icons.js
@@ -0,0 +1,12 @@
+import IconsSVG from './assets/sprite.svg';
+
+function Icons({ name, color, size, className }) {
+
+	return (
+		<svg className={`icon icon-${name} ${className}`} fill={color} stroke={color} width={size} height={size}>
+			<use xlinkHref={`${IconsSVG}#${name}`} />
+		</svg>
+	)
+}
+
+export default Icons;
